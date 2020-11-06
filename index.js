4d06c868a68809ebd0b55e031753b8f94626d079
Guiselle Mejia <guissmejia@gmail.com>
2020-11-05 20:18:02 -0500

    Cors Added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const { config } = require('./config/index');
 
 const employeesAPI = require('./routes/employees-routes');
@@ -11,6 +12,9 @@ const notFoundHandler = require('./utils/middleware/notFoundHandler');
 
 //body parser
 app.use(express.json());
+
+//Cors
+app.use(cors())
 
 employeesAPI(app);
 projectsAPI(app);
