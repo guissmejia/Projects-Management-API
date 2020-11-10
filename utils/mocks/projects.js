36245df55dd41b9b5952dcfb56673e6b7106b7c8
Guiselle Mejia <guissmejia@gmail.com>
2020-11-09 20:26:15 -0500

    The image corresponding to each project is added to the database

--- utils/mocks/projects.js
+++ utils/mocks/projects.js
@@ -1,22 +1,34 @@
 const projectsMock = [
   {
-    name: 'Deportes',
+    id: 1,
+    name: "Deportes",
+    imgSrc: "https://img.icons8.com/windows/32/000000/finish-flag.png",
   },
   {
-    name: 'Automóviles',
+    id: 2,
+    name: "Automóviles",
+    imgSrc: "https://img.icons8.com/fluent/48/000000/car.png",
   },
   {
-    name: 'Construcción',
+    id: 3,
+    name: "Construcción",
+    imgSrc: "https://img.icons8.com/color/48/000000/road-closure.png",
   },
   {
-    name: 'Programación',
+    id: 4,
+    name: "Programación",
+    imgSrc: "https://img.icons8.com/plasticine/100/000000/laptop--v2.png",
   },
   {
-    name: 'Biología',
+    id: 5,
+    name: "Biología",
+    imgSrc: "https://img.icons8.com/color/48/000000/deciduous-tree.png",
   },
   {
-    name: 'Animales',
-  }
+    id: 6,
+    name: "Animales",
+    imgSrc: "https://img.icons8.com/office/16/000000/duck.png",
+  },
 ];
 
 module.exports = {
